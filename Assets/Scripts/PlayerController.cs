cd76c9f8ef5795528c280345ca4f73b713204c6c
aalexhidalgo <alexhidalgocalafat@gmail.com>
2022-01-17 10:41:48 +0100

    P3

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,12 @@ using Random = UnityEngine.Random;
 public class PlayerController : MonoBehaviour
 {
     public bool gameOver;
+
+    public ParticleSystem ExplosionParticleSystem;
+    public ParticleSystem DirtSplattParticleSystem;
+    
     private Animator PlayerAnimator;
+
     private Rigidbody PlayerRigidbody;
     [SerializeField] private float JumpForce = 400f;
     public float GravityModifier = 1f;
@@ -24,29 +29,52 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && IsOnTheGround)
+        if(Input.GetKeyDown(KeyCode.Space) && IsOnTheGround && !gameOver)
         {
             PlayerRigidbody.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
             IsOnTheGround = false;
             PlayerAnimator.SetTrigger("Jump_trig");
+            DirtSplattParticleSystem.Stop();
         }
     }
 
     private void OnCollisionEnter(Collision otherCollider)
     {
-        if (otherCollider.gameObject.CompareTag("Ground"))
+        if (!gameOver)
         {
-            IsOnTheGround = true;
-        }
+            if (otherCollider.gameObject.CompareTag("Ground"))
+            {
+                IsOnTheGround = true;
 
-        if (otherCollider.gameObject.CompareTag("Obstacle"))
-        {
-            //Muerte random
-            int RandomDeath = Random.Range(1, 3);
-            PlayerAnimator.SetBool("Death_b", true);
-            PlayerAnimator.SetInteger("DeathType_int", RandomDeath);
+                //Activamos el sistema de partículas de la tierra
+                DirtSplattParticleSystem.Play();
+            }
 
-            gameOver = true;
+            if (otherCollider.gameObject.CompareTag("Obstacle"))
+            {
+                //Muerte random
+                int RandomDeath = Random.Range(1, 3);
+                PlayerAnimator.SetBool("Death_b", true);
+                PlayerAnimator.SetInteger("DeathType_int", RandomDeath);
+
+                gameOver = true;
+
+                //Si murere se activa la explosión de humo y se desactiva la de tierra al avanzar
+                Vector3 ExplosionOffset = new Vector3(0, 1, 0);
+                Instantiate(ExplosionParticleSystem, transform.position + ExplosionOffset, ExplosionParticleSystem.transform.rotation);
+
+                // ExplosionParticleSystem.Play();
+
+                /*if (IsOnTheGround == true)
+                {
+                    DirtSplattParticleSystem.Stop();
+                }
+                */
+
+                // Destroy(DirtSplattParticleSystem);
+
+                DirtSplattParticleSystem.Stop();
+            }
         }
 
     }
